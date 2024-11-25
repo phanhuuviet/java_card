0b7391605ad35b92657f45716a92da84aeee3770
stormz1302 <namvuxuan284@gmail.com>
2024-11-25 22:58:19 +0700

    update/add-file-jar-SWT

---
+++ src/Popup/Popup_1t1i2b.java
@@ -0,0 +1,22 @@
+package Popup;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class Popup_1t1i2b extends Composite {
+
+	/**
+	 * Create the composite.
+	 * @param parent
+	 * @param style
+	 */
+	public Popup_1t1i2b(Composite parent, int style) {
+		super(parent, style);
+
+	}
+
+	@Override
+	protected void checkSubclass() {
+		// Disable the check that prevents subclassing of SWT components
+	}
+
+}
